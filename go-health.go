20a0606fbfb1adcecc84a3276b2af59e8be55f98
= <dev@nuno.ws>
2016-12-26 22:55:15 +0000

    Added option to export data to one single file

--- go-health.go
+++ go-health.go
@@ -31,17 +31,18 @@ type Record struct {
 	Value         string `xml:"value,attr"`
 }
 
-func types() []string {
+func types() map[string]string {
 
-	types := []string{
-		"HKQuantityTypeIdentifierBodyMass",
-		"HKQuantityTypeIdentifierHeartRate",
-		"HKQuantityTypeIdentifierBodyTemperature",
-		"HKQuantityTypeIdentifierStepCount",
-		"HKQuantityTypeIdentifierDistanceWalkingRunning",
-		"HKQuantityTypeIdentifierActiveEnergyBurned",
-		"HKQuantityTypeIdentifierFlightsClimbed",
-		"HKCategoryTypeIdentifierSleepAnalysis"}
+	types := map[string]string{
+		"HKQuantityTypeIdentifierBodyMass":               "1",
+		"HKQuantityTypeIdentifierHeartRate":              "2",
+		"HKQuantityTypeIdentifierBodyTemperature":        "3",
+		"HKQuantityTypeIdentifierStepCount":              "4",
+		"HKQuantityTypeIdentifierDistanceWalkingRunning": "5",
+		"HKQuantityTypeIdentifierActiveEnergyBurned":     "6",
+		"HKQuantityTypeIdentifierFlightsClimbed":         "7",
+		"HKCategoryTypeIdentifierSleepAnalysis":          "8",
+	}
 
 	return types
 }
@@ -64,10 +65,20 @@ func parseData(file string) (Query, error) {
 }
 
 func recordToCsv(r Record) []string {
-	return []string{r.Type, r.SourceName, r.SourceVersion, r.Device, r.Unit, r.CreationDate, r.StartDate, r.EndDate, r.Value}
+	var v string
+
+	if r.Value == "HKCategoryValueSleepAnalysisAsleep" {
+		v = "1"
+	} else if r.Value == "HKCategoryValueSleepAnalysisInBed" {
+		v = "0"
+	} else {
+		v = r.Value
+	}
+
+	return []string{"", types()[r.Type], r.SourceName, r.SourceVersion, r.Device, r.Unit, r.CreationDate, r.StartDate, r.EndDate, v}
 }
 
-func createCSV(typeName string, record []Record, fromDate string) error {
+func createCSV(all bool, typeName string, record []Record, fromDate string) error {
 	format := "2006-01-02 15:04:05"
 	date, _ := time.Parse(format, fromDate)
 
@@ -84,9 +95,16 @@ func createCSV(typeName string, record []Record, fromDate string) error {
 	for _, r := range record {
 		creationDate, _ := time.Parse(format, strings.Replace(r.CreationDate, " +0000", "", -1))
 
+		var line []string
+
 		if r.Type == typeName && creationDate.After(date) {
-			line := recordToCsv(r)
-			err := writer.Write(line)
+			line = recordToCsv(r)
+		} else if all == true && creationDate.After(date) {
+			line = recordToCsv(r)
+		}
+
+		if line != nil {
+			err = writer.Write(line)
 			if err != nil {
 				return err
 			}
@@ -110,14 +128,25 @@ func main() {
 		panic(err)
 	}
 
-	for _, t := range types() {
+	// create one file per type
+	for t, _ := range types() {
 		fmt.Printf("Exporting %s data... ", t)
-		err := createCSV(t, q.Record, fromDate)
+		err := createCSV(false, t, q.Record, fromDate)
 
 		if err != nil {
 			fmt.Println("Error", err)
 		} else {
 			fmt.Println("done!")
 		}
+	}
+
+	//create one file with all data
+	fmt.Printf("Exporting all data... ")
+	errAll := createCSV(true, "all", q.Record, fromDate)
+
+	if errAll != nil {
+		fmt.Println("Error", errAll)
+	} else {
+		fmt.Println("done!")
 	}
 }
